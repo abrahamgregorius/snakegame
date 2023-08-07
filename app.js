df2296cf35605776c87eaa5c91855f6dda243583
Abraham <athioii6@gmail.com>
2023-08-07 09:57:01 +0700

    interactive movement with arrow keys

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-
+let keyActive 
 const update = () => {
     board.draw()
+    food.draw()
     snake.draw()
 }
 
 update()
-
 setInterval(() => {
+    if(keyActive == 'ArrowDown' && snake.dy !== -1){
+        snake.dx = 0
+        snake.dy = 1
+    }
+    else if(keyActive == 'ArrowLeft' && snake.dx !== 1){
+        snake.dx = -1
+        snake.dy = 0
+    }
+    else if(keyActive == 'ArrowRight' && snake.dx !== -1){
+        snake.dx = 1
+        snake.dy = 0
+    }
+    else if(keyActive == 'ArrowUp' && snake.dy !== 1){
+        snake.dx = 0
+        snake.dy = -1
+    }
+
+
     snake.move()
     update()
-}, 500)
+}, 250)
+
+document.addEventListener('keyup', (event) => {
+    keyActive = event.key
+})
